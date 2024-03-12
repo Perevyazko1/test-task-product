d06e36f3d9a20a97c8a885da4bb3cc418fd3d6d2
Perevyazko <a.perevyazko@gmail.com>
2024-03-12 11:35:34 +0300

    save endpoints rtk

--- src/providers/api/RtkService.tsx
+++ src/providers/api/RtkService.tsx
@@ -7,25 +7,61 @@ import {ProductTypes} from "./models/ProductTypes";
 export const postApi = createApi({
     reducerPath: 'postApi',
     baseQuery: fetchBaseQuery({
-        baseUrl:"http://localhost:8081/",
+        baseUrl: "http://localhost:8081/",
         headers: {
-    'Content-Type': 'application/json'}
+            'Content-Type': 'application/json'
+        }
     }),
     tagTypes: ['Post'],
     endpoints: (build) => ({
-        getData: build.query<ProductTypes[],Args>({
-            query:({param , source})=>({
-                url: `/${source}${param}` ,
+        getData: build.query<ProductTypes[], Args>({
+            query: ({param, source}) => ({
+                url: `/${source}${param}`,
 
-        }),
+            }),
             // providesTags: result => ["Post"]
         }),
-        getUnit: build.query<ProductTypes,Args>({
-            query:({param , source})=>({
-                url: `/${source}${param}` ,
+        getUnit: build.query<ProductTypes, Args>({
+            query: ({param, source}) => ({
+                url: `/${source}${param}`,
 
-        }),
+            }),
             // providesTags: result => ["Post"]
         }),
-})
+        createUnit: build.mutation<ProductTypes, { data: ProductTypes }>({
+            query: ({data}) => ({
+                url: `/productTypes`,
+                method: 'POST',
+                body: {
+                    "packsNumber": data.packsNumber,
+                    "packageType": data.packageType,
+                    "isArchived": data.isArchived,
+                    "description": data.description
+                },
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }),
+            invalidatesTags: ['Post']
+        }),
+        deleteUnit: build.mutation<ProductTypes, { data: ProductTypes }>({
+            query: ({data}) => ({
+                url: `/productTypes`,
+                method: 'DELETE',
+                body: {
+                    "packsNumber": data.packsNumber,
+                    "packageType": data.packageType,
+                    "isArchived": data.isArchived,
+                    "description": data.description
+                },
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }),
+            invalidatesTags: ['Post']
+        }),
+
+    })
 })
